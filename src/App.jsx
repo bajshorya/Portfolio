5c73208f4ef590d322495c85b0820d828253747a
SHORYA BAJ <jain.shorya11@gmail.com>
2024-04-11 16:49:26 +0530

    tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-5xl font-bold underline">Hello world!</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
