29e827988b99358bb00a8596d3e6065c316e82b8
SHORYA BAJ <jain.shorya11@gmail.com>
2024-04-14 00:00:18 +0530

    navbar hover

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="padding-x py-8 lg:px-12 lg:py-6 bg-[#110C1F] text-white">
-      <nav className="flex justify-between items-center max-w-7xl mx-auto">
+      <nav className="flex justify-between items-center max-w-7xl mx-auto ">
         <Link to="/" className="text-3xl ">
           Shorya Baj
         </Link>
 
-        <ul className="flex gap-8 lg:gap-12">
+        <ul className="flex gap-8 lg:gap-12 ">
           <li>
-            <Link to="/about" className="text-lg">
+            <Link to="/about" className="text-lg hover:text-[#896fc1]">
               About
             </Link>
           </li>
           <li>
-            <Link to="/skills" className="text-lg">
+            <Link to="/skills" className="text-lg hover:text-[#896fc1]">
               Skills
             </Link>
           </li>
           <li>
-            <Link to="/projects" className="text-lg">
+            <Link to="/projects" className="text-lg hover:text-[#896fc1]">
               Projects
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="text-lg">
+            <Link to="/contact" className="text-lg hover:text-[#896fc1]">
               Contact
             </Link>
           </li>
