019b11a5fa82c90117ec2040877cc00ee1d4e4d9
SHORYA BAJ <jain.shorya11@gmail.com>
2024-08-01 00:57:34 +0530

    install dependency again and cleared more stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
-import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
