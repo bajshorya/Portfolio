55ec88493777daed02f9c7dc8b64401e70090400
SHORYA BAJ <jain.shorya11@gmail.com>
2024-08-02 22:26:23 +0530

    added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="max-w-2xl  lg:max-w-xl  mx-auto mb-10">
+      <hr className="my-7" />
+      <div className="flex justify-between mt-5">
+        <div className="text-xs text-slate-400 ">
+          © 2022 - 2024 / Shorya Baj
+        </div>
+        <Link to={"https://github.com/bajshorya/Portfolio.git"}>
+          <div className="text-xs text-slate-400">View Source</div>
+        </Link>
+      </div>
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { IntroCard } from "../Components/IntroCard";
 import { Navbar } from "../Components/Navbar";
 import { ProjectSection } from "../Components/ProjectSection";
+import { Footer } from "../Components/Footer";
 
 export const Home = () => {
   return (
-    <div>
-      <div className="max-w-2xl lg:max-w-xl mx-auto">
+    <div className=" home-container">
+      <div className="max-w-2xl lg:max-w-xl mx-auto content">
         <Navbar />
         <IntroCard />
         <ProjectSection />
+        <Footer />
       </div>
     </div>
   );
